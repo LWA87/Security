9e917cd1ed33d5b7aaca9aec49db9385cd45bcca
LWA87 <167525636+LWA87@users.noreply.github.com>
2024-04-19 13:32:34 +0300

    Add files via upload

---
+++ necronbot/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: '',//توكن البوت
+    status: 'discord.gg/nec',//حالة البوت
+    owners: [''],//ايديات الي يمديهم يتحكمون بالحماية
+    //Made By 37d (203584767604686849) For only Necron Host Discord (https://discord.gg/nec)
+}
